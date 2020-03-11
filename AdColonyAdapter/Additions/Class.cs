933c56af14ee7fd53c74fcdfe9fe999d02350d75
Krzus Michał <michal.krzus@iteo.com>
2020-03-11 19:05:25 +0100

    4.1.4 - Init

---
+++ AdColonyAdapter/Additions/Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Com.Google.Ads.Mediation.Adcolony
+{
+    public partial class AdColonyMediationAdapter
+    {
+		public override unsafe void Initialize(global::Android.Content.Context p0, global::Android.Gms.Ads.Mediation.IInitializationCompleteCallback p1, global::System.Collections.Generic.IList<global::Android.Gms.Ads.Mediation.MediationConfiguration> p2)
+		{
+			const string __id = "initialize.(Landroid/content/Context;Lcom/google/android/gms/ads/mediation/InitializationCompleteCallback;Ljava/util/List;)V";
+			IntPtr native_p2 = global::Android.Runtime.JavaList<global::Android.Gms.Ads.Mediation.MediationConfiguration>.ToLocalJniHandle(p2);
+			try
+			{
+				JniArgumentValue* __args = stackalloc JniArgumentValue[3];
+				__args[0] = new JniArgumentValue((p0 == null) ? IntPtr.Zero : ((global::Java.Lang.Object)p0).Handle);
+				__args[1] = new JniArgumentValue((p1 == null) ? IntPtr.Zero : ((global::Java.Lang.Object)p1).Handle);
+				__args[2] = new JniArgumentValue(native_p2);
+				_members.InstanceMethods.InvokeVirtualVoidMethod(__id, this, __args);
+			}
+			finally
+			{
+				JNIEnv.DeleteLocalRef(native_p2);
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Xamarin.Android.GoogleAdsMediation.AdColonyAdapter
+Xamarin android binding google ads mediation AdColony adapter
+
+### Documentation:
+[Google AdMob documentation](https://developers.google.com/admob/android/mediation/adcolony)
